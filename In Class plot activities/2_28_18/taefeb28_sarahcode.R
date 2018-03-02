c9d51caa303c25c241d3b82eabc8f4198b12b4e9
Unknown <sali8599@colorado.edu>
2018-03-02 13:18:57 -0700

    my code

---
+++ In Class plot activities/2_28_18/taefeb28_sarahcode.R
@@ -0,0 +1,10 @@
+
+####
+lakedata <- read_csv("Data Science with R/lakedata.csv")
+lakedata1<-lakedata[,1:5]
+names<-c("Year","GPCC_MAM_Normalized", "CRU_MAM_Normalized", "GPCC_OND_Normalized", "CRU_OND_Normalized")
+names(lakedata1)<-names
+lakedata2<-as.tibble(lakedata1)
+lakedata2<-lakedata2 %>% gather(`GPCC_MAM_Normalized`,`CRU_MAM_Normalized`,`GPCC_OND_Normalized`,`CRU_OND_Normalized`, key='Type',value=`Rainfall`)                                                                                                                                                                                                                                                                                                      
+lakedata4<-lakedata2 %>%separate(Type, into=c("Dataset","Season"),sep="_",extra="drop")
+ggplot(data=lakedata4)+geom_smooth(aes(x=Year,y=rainfall,color=Dataset),se=FALSE)+facet_wrap(~Season)
